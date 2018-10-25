f7815df2d208ab40e1523a935751e77d36904690
Diógenes Dauster <diogenes.dauster@gmail.com>
2018-10-25 18:21:44 -0300

    refactor:update the reduce in a selector data

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -4,67 +4,79 @@ import Page from '../components/page/page'
 import { Link, graphql } from 'gatsby'
 
 const Archive = ({ data }) => {
-  const posts = data.allMarkdownRemark.edges
-
-  const newposts = posts.reduce((previus, current) => {
-    if (previus[current.node.frontmatter.postyear]) {
-      return previus[current.node.frontmatter.postyear].push({
-        postdate: current.node.frontmatter.postdate,
-        slug: current.node.fields.slug,
-        title: current.node.frontmatter.title,
-      })
-    }
-    console.log(current.node.frontmatter.postyear)
-    console.log(previus)
-    return Object.assign(
-      {},
-      {
-        ...previus,
-        [current.node.frontmatter.postyear]: [
-          {
-            postdate: current.node.frontmatter.postdate,
-            slug: current.node.fields.slug,
-            title: current.node.frontmatter.title,
-          },
-        ],
-      }
-    )
-  }, {})
-
-  console.log(newposts)
+  const posts = createPosts(data.allMarkdownRemark.edges)
 
   return (
     <Layout>
       <Page title={'Archives'}>
         {posts
-          ? posts.map((post, idx) => {
-              return (
-                <Fragment key={idx}>
-                  <h1>{post.node.frontmatter.postyear}</h1>
-                  <ul>
-                    <li>
-                      <p>
-                        {post.node.frontmatter.postdate} —{' '}
-                        <Link to={post.node.fields.slug}>
-                          {post.node.frontmatter.title}
-                        </Link>
-                      </p>
-                    </li>
-                  </ul>
-                </Fragment>
-              )
-            })
+          ? Object.keys(posts)
+              .sort((a, b) => b - a)
+              .map(year => {
+                return (
+                  <Fragment key={year}>
+                    <h1>{year}</h1>
+                    <ul>
+                      {posts[year].map((post, idx) => {
+                        return (
+                          <li key={idx}>
+                            <p>
+                              {post.postdate} —{' '}
+                              <Link to={post.slug}>{post.title}</Link>
+                            </p>
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  </Fragment>
+                )
+              })
           : 'No Archives'}
       </Page>
     </Layout>
   )
 }
+
+function createPosts(data) {
+  if (data && data[0].node) {
+    return data.reduce((previus, current) => {
+      const {
+        node: {
+          frontmatter: { postyear, postdate, title },
+          fields: { slug },
+        },
+      } = current
+
+      if (previus[postyear]) {
+        previus[postyear].push({
+          postdate: postdate,
+          slug: slug,
+          title: title,
+        })
+        return previus
+      }
+      return {
+        ...previus,
+        [postyear]: [
+          {
+            postdate: postdate,
+            slug: slug,
+            title: title,
+          },
+        ],
+      }
+    }, {})
+  }
+
+  return null
+}
+
 export default Archive
 
 export const query = graphql`
   query {
     allMarkdownRemark(
-      sort: { fields: frontmatter___date, order: ASC }
+      sort: { fields: frontmatter___date, order: DESC }
       filter: { fields: { layout: { eq: "post" } } }
     ) {
       edges {
